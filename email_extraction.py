512f15b71e68d2c73141df6a60988aa90e3ace98
Stephen Shaw <stephen.shaw@softwire.com>
2019-08-12 11:52:35 +0100

    Implement Part 3.

--- email_extraction.py
+++ email_extraction.py
@@ -16,14 +16,28 @@ def part_two():
     with open('sample.txt') as f:
         samples = f.read()
 
-    results = re.findall(r"[A-Z\.'_%+-]*@softwire.com\b", samples, re.I)
+    results = re.findall(r"\b[A-Z\.'_%+-]*@softwire.com\b", samples, re.I)
 
     return len(results)
 
+def part_three():
+    with open('sample.txt') as f:
+        samples = f.read()
+
+    results = {}
+    groups = re.findall(r"\b[A-Z\.'_%+-]*@([A-Z\.'_%+-]+\.[A-Z'_%+-]*)\b", samples, re.I)
+
+    for g in groups:
+        results[g] = results.get(g, 0) + 1
+    
+    sorted_results = sorted(results.items(), key=lambda kv: -kv[1])
+    report = ''.join([f"  {r[1]}: {r[0]}\n" for r in sorted_results])
+    return report
 
 def main():
     print("Part One results: " + str(part_one()))
     print("Part Two results: " + str(part_two()))
+    print("Part Three results: \n" + part_three())
 
 
 if __name__ == "__main__": main()
